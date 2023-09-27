00d42ce65bd779949d28af66c3415d71b340fb84
Lorne <dev@luotao.net>
2023-09-27 15:45:20 +1000

    faster_whisper tag

---
+++ docker/whisper_faster.py
@@ -0,0 +1,29 @@
+import click as click
+from faster_whisper import WhisperModel
+
+
+@click.command()
+@click.argument('input', type=click.Path(exists=True))
+@click.option('--language', '-l', default='zh', required=True, help='Language')
+@click.option('--model', '-m', default='large-v2', required=True, help='Model file or data')
+def main(input, language, model):
+    if model == 'large':
+        model = 'large-v2'
+    # Run on GPU with FP16
+    # model = WhisperModel(model, device="cuda", compute_type="float16")
+
+    # or run on GPU with INT8
+    # model = WhisperModel(model, device="cuda", compute_type="int8_float16")
+    # or run on CPU with INT8
+    model = WhisperModel(model, device="cpu", compute_type="int8")
+
+    segments, info = model.transcribe(input, language=language, beam_size=5)
+
+    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+
+if __name__ == '__main__':
+    main()
