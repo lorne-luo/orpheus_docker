5496bd3c64b6f5f2ddadec0afdb432b11d54701a
Lorne <dev@luotao.net>
2023-09-28 01:21:16 +1000

    update

--- docker/whisper-jax.py
+++ docker/whisper-jax.py
@@ -1,19 +1,30 @@
 import click
-
+from datetime import *
 from whisper_jax import FlaxWhisperPipline
 
 
 @click.command()
 @click.argument('input', type=click.Path(exists=True))
 @click.option('--language', '-l', default='zh', required=False, help='Language')
-@click.option('--model', '-m', default='large-v2', required=False, help='Model file or data')
+@click.option('--model', '-m', default='medium', required=False, help='Model file or data')
 def main(input, language, model):
+    start=datetime.now()
     # instantiate pipeline
     pipeline = FlaxWhisperPipline(f"openai/whisper-{model}")
 
-    # used cached function thereafter - super fast!!
+    # JIT compile the forward call - slow, but we only do once
+    print('JIT compile the forward call - slow, but we only do once')
     text = pipeline(input, language=language, return_timestamps=True)
     print(text)
+    print(datetime.now()-start)
+
+    start=datetime.now()
+    # used cached function thereafter - super fast!!
+    print('used cached function thereafter - super fast!!')
+    text = pipeline(input, language=language, return_timestamps=True)
+    print(text)
+    print(datetime.now()-start)
+
 
 if __name__ == '__main__':
     main()
